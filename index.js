e33cf48d84c08f1e63b67ce470dfabae16745af4
amira qasim <104715002+Miraa8@users.noreply.github.com>
2024-02-21 09:45:19 +0200

    Update index.js

--- index.js
+++ index.js
@@ -17,13 +17,7 @@ const port = 3000;
 //connect db
 await connectDb();
 //cors
-const corsConfig = {
-  origin:"*",
-  credential:true,
-  methods:["POST","PUT","PATCH","GET","DELETE"]
-}
-app.options("",cors(corsConfig));
-app.use(cors(corsConfig));
+app.use(cors());
 // const whiteList = ["http://127.0.0.1:5500"];
 // app.use((req, res, next) => {
 //   if(req.originalUrl.includes("/auth/activate_account")){
