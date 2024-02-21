72a997bf39b413a43bbb132dcb231cf6d8966247
amira qasim <104715002+Miraa8@users.noreply.github.com>
2024-02-21 08:50:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ import productRouter from "./src/modules/product/product.router.js";
 import cartRouter from "./src/modules/cart/cart.router.js";
 import orderRouter from "./src/modules/order/order.router.js";
 import reviewRouter from "./src/modules/review/review.router.js";
+import cors from "cors";
 dotenv.config();
 
 const app = express();
@@ -17,22 +18,23 @@ const port = 3000;
 //connect db
 await connectDb();
 //cors
-const whiteList = ["http://127.0.0.1:5500"];
-app.use((req, res, next) => {
-  if(req.originalUrl.includes("/auth/activate_account")){
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.setHeader("Access-Control-Allow-Methods", "GET");
-    return next();
-  }
-  if (!whiteList.includes(req.header("origin"))) {
-    return next(new Error("blocked by CORS!"));
-  }
-  res.setHeader("Access-Control-Allow-Origin","*");
-  res.setHeader("Access-Control-Allow-Headers","*");
-  res.setHeader("Access-Control-Allow-Methods","*");
-  res.setHeader("Access-Control-Allow-Private-Network",true);
-  return next()
-});
+app.use(cors);
+// const whiteList = ["http://127.0.0.1:5500"];
+// app.use((req, res, next) => {
+//   if(req.originalUrl.includes("/auth/activate_account")){
+//     res.setHeader("Access-Control-Allow-Origin", "*");
+//     res.setHeader("Access-Control-Allow-Methods", "GET");
+//     return next();
+//   }
+//   if (!whiteList.includes(req.header("origin"))) {
+//     return next(new Error("blocked by CORS!"));
+//   }
+//   res.setHeader("Access-Control-Allow-Origin","*");
+//   res.setHeader("Access-Control-Allow-Headers","*");
+//   res.setHeader("Access-Control-Allow-Methods","*");
+//   res.setHeader("Access-Control-Allow-Private-Network",true);
+//   return next()
+// });
 //parsing
 app.use(express.json());
 
