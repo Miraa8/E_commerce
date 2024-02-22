5d6f03b95c4153ee55946b917fc98c05ae8c8958
amira qasim <amirakassem855@gmail.com>
2024-02-22 21:58:09 +0200

    fixing errors

--- DB/connection.js
+++ DB/connection.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDb = async () => {
   return await mongoose
-    .connect(process.env.connection_url)
+    .connect(process.env.CONNECTION_URL)
     .then(() => console.log("DB connected successfully"))
     .catch(() => console.log("failed to connect DB!"));
 };
